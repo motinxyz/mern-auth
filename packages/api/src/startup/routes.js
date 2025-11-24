40841aedd4fbc61975495d1e7590e5aa04dce6af
motinxyz <motinsghs@gmail.com>
2025-11-24 18:48:45 +0600

    feat: add endpoint for sending test emails

--- packages/api/src/startup/routes.js
+++ packages/api/src/startup/routes.js
@@ -1,9 +1,11 @@
 import { v1Routes } from "../routes/v1/index.js";
 import webhookRoutes from "../routes/webhooks.routes.js";
+import testEmailRoutes from "../routes/test-email.routes.js";
 
 const apiPrefixV1 = "/api/v1";
 
 export const configureRoutes = (app) => {
   app.use(apiPrefixV1, v1Routes);
   app.use("/webhooks", webhookRoutes);
+  app.use("/test-email", testEmailRoutes);
 };
